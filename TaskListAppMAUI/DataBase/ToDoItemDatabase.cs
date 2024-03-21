7e10721b7b2ca204439e7daf5a0ef31274640d55
VMekh8 <mekhvitaliy88@gmail.com>
2024-03-21 21:14:33 +0200

    Create a DataBase class

---
+++ TaskListAppMAUI/DataBase/ToDoItemDatabase.cs
@@ -0,0 +1,52 @@
+﻿using SQLite;
+using TaskListAppMAUI.Model;
+
+namespace TaskListAppMAUI.DataBase
+{
+    public class ToDoItemDatabase
+    {
+        private SQLiteAsyncConnection _dataBase;
+
+        async Task Init()
+        {
+            if (_dataBase is not null)
+            {
+                return;
+            }
+            else
+            {
+                _dataBase = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+                await _dataBase.CreateTableAsync<ToDoModel>();
+            }
+        }
+
+        public async Task<List<ToDoModel>> GetItemsAsync()
+        {
+            await Init();
+            return await _dataBase.Table<ToDoModel>().ToListAsync();
+        }
+
+        public async Task<ToDoModel> GetItemByIdAsync(int id)
+        {
+            await Init();
+            return await _dataBase.Table<ToDoModel>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<int> SaveItemAsync(ToDoModel item)
+        {
+            await Init();
+            if (item.Id != 0)
+                return await _dataBase.UpdateAsync(item);
+            else
+                return await _dataBase.InsertAsync(item);
+        }
+
+        public async Task<int> DeleteItemAsync(ToDoModel item)
+        {
+            await Init();
+            return await _dataBase.DeleteAsync(item);
+        }
+    }
+}
+
+
