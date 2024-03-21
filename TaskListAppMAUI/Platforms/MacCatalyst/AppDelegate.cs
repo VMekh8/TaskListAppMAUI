b08f2ab114abbdf3cf513323f751f8f3248e3ff4
VMekh8 <mekhvitaliy88@gmail.com>
2024-03-21 20:15:00 +0200

    Add project files.

---
+++ TaskListAppMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TaskListAppMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
